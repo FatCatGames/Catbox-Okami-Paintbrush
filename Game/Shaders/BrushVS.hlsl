efc4c91c90df639c17d87648d67e12cfe721eeb8
FatCatGames <51986299+FatCatGames@users.noreply.github.com>
2023-03-21 10:31:20 +0100

    Brush looks good now!

--- Game/Shaders/BrushVS.hlsl
+++ Game/Shaders/BrushVS.hlsl
@@ -12,27 +12,26 @@ VertexToPixel main(VertexInput input)
 	VertexToPixel result;
 
 
-	float influence = pow(input.VxColor.r, 0.3f);
+	float brush = pow(input.VxColor.r, 0.3f);
+	float brushTip = pow(input.VxColor.r, 2);
 
 	//Moves brush up/down when painting
-	input.Position.y += 5 * strength;
+	input.Position.y += 10 * strength;
 	
 	//Accidental squash/stretch!
-	//input.Position.y *= 1 + strength;
-	//input.Position.y *= 1 - strength * 0.5f;
+	input.Position.y *= 1 + strength;
+	input.Position.y *= 1 - strength * 0.5f;
 
 	//Squash the brush
-	input.Position.y -= (strength * influence) * 5;
-	input.Position.y -= (strength * pow(influence,2));
-	//input.Position.y -= (strength * pow(input.VxColor.r, 10)) * 1.f;
-	input.Position.xz *= 1 + strength * influence * 0.25f;
-	input.Position.xz *= 1 + strength * pow(influence, 2) * 0.25f;
+	input.Position.y -= (strength * brush) * 5;
+	input.Position.y -= (strength * pow(brushTip,2)) * 3;
+	input.Position.xz *= 1 + strength * brush * 0.3f;
 
 
 	float4 vertexWorldPosition = mul(OB_ToWorld, input.Position);
 	
 	float brushOffset = pow((input.VxColor.r), 0.7f);
-	vertexWorldPosition.xz += 0.07f * strength * paintDir.xz * brushOffset;
+	vertexWorldPosition.xz += 0.15f * strength * paintDir.xz * brushTip;
 
 	const float4 vertexViewPosition = mul(FB_ToView, vertexWorldPosition);
 	const float4 vertexProjectionPosition = mul(FB_ToProjection, vertexViewPosition);
