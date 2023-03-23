9d33d6cf15080ee4d9dc436c0bb5a2dce0fcbcb3
FatCatGames <51986299+FatCatGames@users.noreply.github.com>
2023-03-23 11:57:33 +0100

    flower trail tweak

--- Game/FlowerTrail.cpp
+++ Game/FlowerTrail.cpp
@@ -8,13 +8,17 @@ void FlowerTrail::Awake()
 	mySizeCurve.AddCoord({ 0.7f, 1.f });
 	mySizeCurve.AddCoord({ 1.f, 0.f });
 
-	myTransform->Translate(Vector3f::right() * Catbox::GetRandom(-0.2f, 0.2f));
-	myTransform->Translate(Vector3f::forward() * Catbox::GetRandom(-0.2f, 0.2f));
-	mySizeMultiplier = Catbox::GetRandom(0.8f, 1.f);
+	mySizeMultiplier = Catbox::GetRandom(0.7f, 1.f);
 }
 
 void FlowerTrail::Update()
 {
+	if (!myHasAppliedOffset)
+	{
+		myTransform->Translate(Vector3f::right() * Catbox::GetRandom(-0.5f, 0.5f));
+		myTransform->Translate(Vector3f::forward() * Catbox::GetRandom(-0.5f, 0.5f));
+		myHasAppliedOffset = true;
+	}
 	myCurrentLifeTime += deltaTime;
 	myTransform->SetWorldScale(Vector3f::one() * mySizeCurve.Evaluate(myCurrentLifeTime / myTargetLifetime) * mySizeMultiplier);
 
--- Game/FlowerTrail.h
+++ Game/FlowerTrail.h
@@ -9,6 +9,7 @@ public:
 
 private:
 	Curve mySizeCurve;
+	bool myHasAppliedOffset = false;
 	float myCurrentLifeTime = 0;
 	float myTargetLifetime = 2;
 	float mySizeMultiplier = 1;
